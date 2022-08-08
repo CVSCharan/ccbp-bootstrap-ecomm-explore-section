8fff0629128dde3b3e98c1dd4d6612ee48b5f5d6
CVSCharan <charan.cvs@gmail.com>
2022-08-08 20:55:00 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-container">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="h1 mt-md-5 mb-md-5">Explore Deals & Offers</h1>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="card1 shadow">
+              <h1 className="h2">Up to 80% off | Elecetonics & Gadgets</h1>
+              <div className="d-flex flex-row justify-content-center text-center">
+                <div>
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-mamba-watch-img.png"
+                    className="img1"
+                    alt="img4"
+                  />
+                  <h1 className="h3">Smart Watch</h1>
+                </div>
+                <div>
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-camera-img.png"
+                    className="img1"
+                    alt="img5"
+                  />
+                  <h1 className="h3">Cameras</h1>
+                </div>
+              </div>
+              <div className="d-flex flex-row justify-content-center text-center">
+                <div>
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-sport-and-fitness-img.png"
+                    className="img1"
+                    alt="img6"
+                  />
+                  <h1 className="h3">Sports & Fitness</h1>
+                </div>
+                <div>
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-ear-phones-pink-img.png"
+                    className="img1"
+                    alt="img7"
+                  />
+                  <h1 className="h3">Earphones</h1>
+                </div>
+              </div>
+              <a className="p1" href="!#">
+                See All Details{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16px"
+                  height="16px"
+                  fill="#ff9f00"
+                  className="bi bi-arrow-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="card2 shadow">
+              <h1 className="h2">Festival Offers TVs & appliances</h1>
+              <div className="d-flex flex-row justify-content-center">
+                <img
+                  src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-tv-img.png"
+                  className="img2"
+                  alt="img8"
+                />
+              </div>
+              <a className="p1" href="!#">
+                See All Details{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16px"
+                  height="16px"
+                  fill="#ff9f00"
+                  className="bi bi-arrow-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="card2 shadow">
+              <h1 className="h2">Under Rs.4000 | Friendly Accessories</h1>
+              <div className="d-flex flex-row justify-content-center">
+                <div className="text-center">
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-tablet-device-img.png"
+                    className="img3"
+                    alt="img9"
+                  />
+                  <h1 className="h3">Tablet</h1>
+                </div>
+                <div className="text-center">
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-joy-stick-img.png"
+                    className="img3"
+                    alt="img1"
+                  />
+                  <h1 className="h3">Play Station</h1>
+                </div>
+              </div>
+
+              <div className="d-flex flex-row justify-content-center">
+                <div className="text-center">
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-speakers-big-img.png"
+                    className="img3"
+                    alt="img2"
+                  />
+                  <h1 className="h3">Speakers</h1>
+                </div>
+                <div className="text-center">
+                  <img
+                    src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-explore-vr-set-img.png"
+                    className="img3"
+                    alt="img3"
+                  />
+                  <h1 className="h3">Virtual Reality Set</h1>
+                </div>
+              </div>
+
+              <a className="p1" href="!#">
+                See All Details{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16px"
+                  height="16px"
+                  fill="#ff9f00"
+                  className="bi bi-arrow-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
